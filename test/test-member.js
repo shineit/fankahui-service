037cc9eb2a9227f4721474d63d9a4a31ba6008a1
Guan Bo <guanbo2002@gmail.com>
2014-07-04 00:56:12 +0800

    Add: fixtures

--- test/test-member.js
+++ test/test-member.js
@@ -2,17 +2,21 @@ var qs = require('qs');
 var RestAgent = require('./support/restagent')
 var Member = new RestAgent('members')
 
+var member = require('./fixtures/members.json')[0]
 
 var query = {
  where: {
    price: {gt: 2}
  }
-};
+}
 
 describe('Member', function(){
   describe('# POST', function(){
     it('should success create one', function(done){
-      done()
+      Member.post({json: member}, function (err, res, body) {
+        res.statusCode.should.equal(200)
+        done()
+      })
     })
   })
   
